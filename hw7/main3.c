5d8ed05ca016f98d6cf318b33854ff58698f43af
Spectre0304 <100345364+Spectre0304@users.noreply.github.com>
2022-12-28 20:19:16 +0800

    HW#7

--- hw7/main3.c
+++ hw7/main3.c
@@ -2,8 +2,8 @@
 
 int main(){
     for(int y=1,x=1; y<=9 ;x++){
-    printf("%d*%d=%d\t",y,x,y*x);
-    x<9 ? x:(x=0,y++,printf("\n"));
+        printf("%d*%d=%d\t",y,x,y*x);
+        x<9 ? x:(x=0,y++,printf("\n"));
     }
 return 0;
 }
